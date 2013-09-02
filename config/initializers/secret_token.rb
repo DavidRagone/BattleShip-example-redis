fca4776629d0418cd4c1f3c8254ccadf1481e63a
DavidRagone <david@ouya.tv>
2013-09-02 13:27:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BattleShipRedis::Application.config.secret_key_base = '6ee07df6c1a5578c664736864095b05eeb60a80e1e7c90e21e464c627ae8d69835d2f8ce9cb66806d2bc5222d8febeab37a8dfc01d648145dc62ffa098bd6e79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BattleShipRedis::Application.config.session_store :cookie_store, key: '_BattleShip-Redis_session'
