160c5778481021e9e3b9ab113e918010d1d8f1c1
DavidRagone <david@ouya.tv>
2013-09-05 00:00:05 -0700

    Rescue block for redis failures

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,20 +3,25 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
-    Signal.trap 'TERM' do
-        puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-        Process.kill 'QUIT', Process.pid
-    end
+  Signal.trap 'TERM' do
+      puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+      Process.kill 'QUIT', Process.pid
+  end
 
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  begin
     defined?(Redis) and Redis.current.quit
+  rescue
+    Redis.current.instance_variable_set(:@client, Redis::Client.new(url: ENV['redis']))
+    Redis.current.quit
+  end
 end
 
 after_fork do |server, worker|
-    Signal.trap 'TERM' do
-        puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-    end
+  Signal.trap 'TERM' do
+      puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
 
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-    defined?(Redis) and Redis.new(url: ENV['redis'])
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  defined?(Redis) and Redis.new(url: ENV['redis'])
 end
